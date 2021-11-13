b3c03509eb79a955d067b5334341094edaa061e1
Meherab Hossain <60975003+meherabbappy@users.noreply.github.com>
2021-11-13 21:55:34 +0600

    add

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 // middlewaer
 app.use(cors())
+
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9qap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
